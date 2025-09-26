3ef493e050f2795ab1cd7c5a6379422f1d8e0a16
Omkar64-coder <omkarmandadkar64@gmail.com>
2025-09-27 01:59:37 +0530

    Add files via upload

---
+++ StudentController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Practi5.Controllers
+{
+    public class StudentController : ApiController
+    {
+        static List<Student> Students = new List<Student>();
+        // GET: api/Student
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            return Students;
+        }
+
+        // GET: api/Student/5
+        public Student Get(int id)
+        {
+            return Students.FirstOrDefault(s => s.id == id);
+        }
+
+        // POST: api/Student
+        public void Post([FromBody]Student value)
+        {
+            Students.Add(value);
+        }
+
+        // PUT: api/Student/5
+        public void Put(int id, [FromBody]Student value)
+        {
+            int i = Students.FindIndex(s => s.id == id);
+            if (i >= 0)
+                Students[i] = value;
+        }
+
+        // DELETE: api/Student/5
+        public void Delete(int id)
+        {
+            Students.RemoveAll(s => s.id == id);
+        }
+    }
+}
